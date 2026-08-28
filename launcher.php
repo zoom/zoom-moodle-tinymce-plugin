00ffada76c99605a3774809d291cc43bf92b33ae
Corey Mead <corey.mead@zoom.us>
2026-08-28 14:05:15 -0400

    Fix launcher locallib include for configured tool guard

--- launcher.php
+++ launcher.php
@@ -23,6 +23,7 @@
  */
 
 require_once(__DIR__ . '/../../../../../config.php');
+require_once(__DIR__ . '/locallib.php');
 require_once($CFG->dirroot . '/mod/lti/lib.php');
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
 
